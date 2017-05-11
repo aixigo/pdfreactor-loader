8f27dc8e8a64fa3fb8eb1d46afa6880a9527d318
Jonas Pommerening <jonas.pommerening@gmail.com>
2017-05-11 16:09:38 +0200

    PDFreactor loader – initial version

---
+++ index.js
@@ -0,0 +1,99 @@
+/**
+ * Copyright 2017 aixigo AG
+ * Released under the MIT license.
+ * https://opensource.org/licenses/MIT
+ */
+
+'use strict';
+
+const http = require( 'http' );
+const path = require( 'path' );
+const java = require( 'java' );
+const portfinder = require( 'portfinder' );
+const loaderUtils = require( 'loader-utils' );
+
+const CONFIGURATION = 'com.realobjects.pdfreactor.Configuration';
+const PDFREACTOR = 'com.realobjects.pdfreactor.PDFreactor';
+
+module.exports = function (source) {
+   const options = loaderUtils.getOptions( this ) || {};
+   const classpath = Array.isArray( options.classpath ) ? options.classpath : [ options.classpath ];
+
+   this.cacheable();
+   this.async();
+
+   if( !java.isJvmCreated() ) {
+      java.options.push('-Djava.awt.headless=true');
+      java.registerClient( () => {
+         classpath.forEach( cp => {
+            if( cp ) {
+               java.classpath.push( path.resolve( this.context, cp ) );
+            }
+         } );
+      } );
+   }
+
+   java.ensureJvm( () => {
+      const pdfconfig = java.newInstanceSync( CONFIGURATION );
+      const pdfrenderer = java.newInstanceSync( PDFREACTOR );
+      const server = http.createServer( ( req, res ) => {
+         const resource = path.join( this.context, req.url.substr( 1 ) );
+         this.addDependency( resource );
+         this.fs.readFile( resource, ( err, content ) => {
+            if( err ) {
+               res.statusCode = 404;
+               res.statusMessage = 'Not found';
+               res.end( err.message );
+               return;
+            }
+
+            res.statusCode = 200;
+            res.statusMessage = 'Ok';
+            res.end( content );
+         } );
+      } );
+
+      pdfconfig.setEnableDebugModeSync( true );
+      pdfconfig.setAddLinks( true );
+      pdfconfig.setAddBookmarks( true );
+      pdfconfig.setDocument( source );
+
+      pipe( [
+         ( callback ) => { portfinder.getPort( callback ); },
+         ( port, callback ) => {
+            pdfconfig.setBaseURL( 'http://127.0.0.1:' + port );
+            server.listen( port, callback );
+         },
+         ( callback ) => {
+            pdfrenderer.convertAsBinary( pdfconfig, callback );
+         },
+         ( bytes, callback ) => {
+            callback( null, Buffer.from( bytes ) );
+         }
+      ], ( err, buffer ) => {
+         this.callback( err, buffer );
+         server.close();
+      } );
+   } );
+};
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function pipe( fns, callback ) {
+   const stack = fns.slice();
+
+   iterate( null );
+
+   function iterate( err ) {
+      const fn = stack.shift();
+
+      if( err || !fn ) {
+         callback.apply( null, arguments );
+      }
+      else {
+         fn.apply( null, Array.prototype.slice.call( arguments, 1, fn.length ).concat( [ iterate ] ) );
+      }
+   }
+};
+
+
