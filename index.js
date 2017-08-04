94d1946edd46166a4aa3ed98f49caed274e9a18a
Jonas Pommerening <jonas.pommerening@gmail.com>
2017-08-04 12:08:08 +0200

    (#6) add plugin for sharing server between runs

--- index.js
+++ index.js
@@ -3,19 +3,35 @@
  * Released under the MIT license.
  * https://opensource.org/licenses/MIT
  */
-const http = require( 'http' );
 const path = require( 'path' );
 const java = require( 'java' );
-const portFinder = require( 'portfinder' );
+const http = require( 'http' );
+const crypto = require( 'crypto' );
 const loaderUtils = require( 'loader-utils' );
 const createLogWriter = require( './lib/log-writer' );
+const createServer = require( './lib/server' );
+const PdfReactorServerPlugin = require( './plugin' );
 
+const BASE64 = 'base64';
 const CONFIGURATION = 'com.realobjects.pdfreactor.Configuration';
+const KEY_VALUE_PAIR = CONFIGURATION + '$KeyValuePair';
 const PDF_REACTOR = 'com.realobjects.pdfreactor.PDFreactor';
 const LEVEL = 'java.util.logging.Level';
 const LOGGER = 'java.util.logging.Logger';
 const LOGGING_HANDLER = 'com.aixigo.pdfreactor_loader.LoggingHandler';
 const LOG_WRITER = 'com.aixigo.pdfreactor_loader.LogWriter';
+
+const NONCE_HEADER = 'X-PDFreactor-Loader-Nonce';
+const LOGGER_ERROR_MESSAGE = ( // eslint-disable-next-line indent
+`-----------------------------------------------------------
+   pdfreactor-loader: Failed to create and set logger.
+   Most probably the reason for this are missing classpath entries for the java classes of this module.
+   This can happen, if the jvm was already running before this module was loaded.
+   You can fix this manually by adding the entries yourself:
+
+      java.classpath.push( ...require( 'pdfreactor-loader/classpath' ) );
+-----------------------------------------------------------`
+);
 
 if( !java.isJvmCreated() ) {
    java.classpath.push( ...require( './classpath' ) );
@@ -23,7 +39,16 @@ if( !java.isJvmCreated() ) {
 
 module.exports = function( source ) {
    const options = loaderUtils.getOptions( this ) || {};
+   const context = options.context ?
+      path.resolve( this.context, options.context ) :
+      ( this.options.context || this.context );
+   const server = createServer( this.options.server || {} );
    const classpath = Array.isArray( options.classpath ) ? options.classpath : [ options.classpath ];
+   const hash = crypto.createHash( 'sha256' );
+
+   hash.update( source );
+   hash.update( context );
+   hash.update( this.request );
 
    this.cacheable();
    this.async();
@@ -31,66 +56,88 @@ module.exports = function( source ) {
    if( !java.isJvmCreated() ) {
       java.options.push('-Djava.awt.headless=true');
       java.registerClient( () => {
-         java.classpath.push(
-            ...classpath.filter( cp => !!cp ).map( cp => path.resolve( this.context, cp ) )
-         );
+         classpath.forEach( cp => {
+            if( cp ) {
+               java.classpath.push( cp );
+            }
+         } );
       } );
    }
 
    java.ensureJvm( () => {
       const pdfConfig = java.newInstanceSync( CONFIGURATION );
       const pdfRenderer = java.newInstanceSync( PDF_REACTOR );
+      const loaderNonce = hash.digest( BASE64 );
 
-      const server = http.createServer( ( req, res ) => {
-         const resource = path.join( this.context, req.url.substr( 1 ) );
-         this.addDependency( resource );
-         this.fs.readFile( resource, ( err, content ) => {
-            if( err ) {
-               res.statusCode = 404;
-               res.statusMessage = 'Not found';
-               res.end( err.message );
-               return;
-            }
+      const requestHandler = ( req, res, next ) => {
+         if( req.headers[ NONCE_HEADER.toLowerCase() ] === loaderNonce ) {
+            const resource = path.resolve( context, req.url.substr( 1 ) );
+            this.addDependency( resource );
+            this.fs.readFile( resource, ( err, content ) => {
+               if( err ) {
+                  next();
+                  return;
+               }
 
-            res.statusCode = 200;
-            res.statusMessage = 'Ok';
-            res.end( content );
-         } );
-      } );
-
-      try {
-         pdfConfig.setLoggerSync( createLogger() );
-      }
-      catch( e ) {
-         /* eslint-disable no-console */
-         console.warn( `-----------------------------------------------------------
-   pdfreactor-loader: Failed to create and set logger.
-   Most probably the reason for this are missing classpath entries for the java classes of this module.
-   This can happen, if the jvm was already running before this module was loaded.
-   You can fix this manually by adding the entries yourself:
-   
-      java.classpath.push( ...require( 'pdfreactor-loader/classpath' ) );
------------------------------------------------------------` );
-         console.info( '   Underlying Exception:', e );
-         /* eslint-enable no-console */
-      }
-      pdfConfig.setLogLevelSync( java.import( CONFIGURATION ).LogLevel.DEBUG );
-      pdfConfig.setEnableDebugModeSync( true );
-      pdfConfig.setJavaScriptModeSync( java.import( CONFIGURATION ).JavaScriptMode.ENABLED );
-      pdfConfig.setAddLinksSync( true );
-      pdfConfig.setAddBookmarksSync( true );
-      pdfConfig.setDocumentSync( source );
+               res.statusCode = 200;
+               res.statusMessage = 'Ok';
+               res.end( content );
+            } );
+         }
+         else {
+            next();
+         }
+      };
 
       pipe( [
          callback => {
-            listen( server, callback );
+            try {
+               //pdfConfig.setLoggerSync( createLogger() );
+            }
+            catch( e ) {
+               /* eslint-disable no-console */
+               console.warn( LOGGER_ERROR_MESSAGE );
+               console.info( '   Underlying Exception:', e );
+               /* eslint-enable no-console */
+            }
+            pdfConfig.setLogLevelSync( java.import( CONFIGURATION ).LogLevel.DEBUG );
+            pdfConfig.setEnableDebugModeSync( true );
+            pdfConfig.setJavaScriptModeSync( java.import( CONFIGURATION ).JavaScriptMode.ENABLED );
+            pdfConfig.setAddLinksSync( true );
+            pdfConfig.setAddBookmarksSync( true );
+            pdfConfig.setDocumentSync( source );
+            pdfConfig.setRequestHeadersSync(
+               java.newInstanceSync( KEY_VALUE_PAIR, NONCE_HEADER, loaderNonce ) );
+
+            server.use( requestHandler );
+            server.listen( callback );
          },
-         ( port, callback ) => {
-            pdfConfig.setBaseURL( 'http://127.0.0.1:' + port );
+         callback => {
+            const address = server.address();
+            pdfConfig.setBaseURLSync( `http://localhost:${address.port}` );
             pdfRenderer.convertAsBinary( pdfConfig, callback );
          },
          ( bytes, callback ) => {
-            callback( null, Buffer.from( bytes ) );
+            const buffer = Buffer.from( bytes );
+            const entry = {
+               name: path.basename( this.resourcePath, path.extname( this.resourcePath ) ),
+               time: Date.now(),
+               nonce: loaderNonce,
+               data: [
+                  {
+                     type: 'application/pdf',
+                     encoding: BASE64,
+                     data: buffer.toString( BASE64 )
+                  },
+                  {
+                     type: 'text/html',
+                     encoding: BASE64,
+                     data: Buffer.from( source ).toString( BASE64 )
+                  }
+               ]
+            };
+
+            postResult( server.address(), entry, err => callback( err, buffer ) );
          }
       ], ( err, buffer ) => {
          this.callback( err, buffer );
@@ -120,31 +167,7 @@ module.exports = function( source ) {
    }
 };
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-function listen( server, callback ) {
-   let tries = 3;
-
-   repeat();
-
-   function repeat() {
-      portFinder.getPort( ( err, port ) => {
-         if( err ) {
-            callback(err);
-         }
-         else {
-            server.listen( port, err => {
-               if(err && tries-- > 0) {
-                  setTimeout( repeat, 100 + (Math.random() * 100) );
-               }
-               else {
-                  callback( err, port );
-               }
-            } );
-         }
-      } );
-   }
-}
+module.exports.plugin = PdfReactorServerPlugin;
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -165,4 +188,19 @@ function pipe( fns, callback ) {
    }
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+function postResult( address, entry, callback ) {
+   const req = http.request( {
+      method: 'POST',
+      port: address.port,
+      family: address.family,
+      path: '/',
+      headers: {
+         'content-type': 'application/json'
+      }
+   }, () => callback( null ) );
+   req.on( 'error', callback );
+   req.write( JSON.stringify( entry ) );
+   req.end();
+}
---
+++ plugin.js
@@ -0,0 +1,80 @@
+/**
+ * Copyright 2017 aixigo AG
+ * Released under the MIT license.
+ * https://opensource.org/licenses/MIT
+ */
+const { bold } = require( 'colors/safe' );
+const path = require( 'path' );
+const createServer = require( './lib/server' );
+const createApp = require( './lib/app' );
+const loaderPath = require.resolve( __dirname );
+
+module.exports = function( options ) {
+   const server = createServer( options || {} );
+
+   return {
+      apply( compiler ) {
+         const context = options && options.context || compiler.context;
+         const middleware = [];
+
+         if( Array.isArray( options.middleware ) ) {
+            middleware.push.apply( middlware, options.middleware );
+         }
+         else if( 'middlware' in options ) {
+            middleware.push( options.middleware );
+         }
+         else {
+            middlware.push( createApp( options ) );
+         }
+
+         compiler.plugin( 'after-environment', () => {
+            middleware.unshift( requestHandler( compiler.inputFileSystem, context ) );
+            middleware.forEach( server.use );
+         } );
+         compiler.plugin( 'watch-run', ( watcher, callback ) => {
+            server.listen( err => {
+               const baseUrl = `http://localhost:${server.address().port}/`;
+               if( !err ) {
+                  // eslint-disable-next-line no-console
+                  console.log( `PDFreactor server is running at ${bold( baseUrl )}` );
+               }
+               callback( err );
+            } );
+         } );
+         compiler.plugin( 'watch-close', ( watcher, callback ) => {
+            server.close();
+            callback();
+         } );
+         compiler.plugin( 'before-run', ( compiler, callback ) => {
+            server.listen( callback );
+         } );
+         compiler.plugin( 'compilation', compilation => {
+            compilation.plugin( 'normal-module-loader', ( loaderContext, module ) => {
+               if( module.loaders.some( obj => obj.loader === loaderPath ) ) {
+                  loaderContext.options = Object.assign(
+                     loaderContext.options || {},
+                     options,
+                     { server, context }
+                  );
+               }
+            } );
+         } );
+      }
+   };
+};
+
+function requestHandler( fs, context ) {
+   return ( req, res, next ) => {
+      const resource = path.resolve( context, req.url.replace( /^\//, '' ) );
+      fs.readFile( resource, ( err, content ) => {
+         if( err ) {
+            next();
+            return;
+         }
+
+         res.statusCode = 200;
+         res.statusMessage = 'Ok';
+         res.end( content );
+      } );
+   };
+}
